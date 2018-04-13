f376e7787761d519054187cbad9cc2ecaa799597
user.name <Awais_ijaz786@hotmail.com>
2018-04-13 13:40:58 +0100

    Second commit

--- Java Script Projects/Exercise Book Tasks/functions.js
+++ Java Script Projects/Exercise Book Tasks/functions.js
@@ -2,3 +2,13 @@ function myFunction(p1, p2) {
     return p1 * p2;
 }
 var x = myFunction(4, 3); //12
+
+
+do {y = y / 3;}
+while (y % 3 === 0 && y > 1 );
+
+do {y = (y - 1) / 3;}
+while (y % 3 === 1 && y > 1);
+
+do {y = (y + 1) / 3;}
+while (y % 3 === 2 && y > 1);
